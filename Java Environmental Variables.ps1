0a8034ca47ac431a9c040e09fb78504c42d1851d
Justin Thorpe <justint@axosoft.com>
2018-05-02 06:44:36 -0700

    Initial Upload to GH
    2 Scripts

---
+++ Java Environmental Variables.ps1
@@ -0,0 +1,6 @@
+﻿Set-Location Env:
+Get-ChildItem
+$java = Read-Host 'Please insert Java Path here before bin folder'
+Set-Item -Path Env:JAVA_HOME -Value ($Env:JAVA_HOME + "$java;")
+Set-Item -Path Env:PATHEXT -Value ($Env:PATHEXT + "$java\bin;")
+Set-Item -Path Env:PATH -Value ($Env:PATH + "$java\bin;")
---
+++ RCBdeath.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /data
+rm -rf mysetup
+rm -rf unitsetup
+rm -rf childsetup
+cd /etc
+rm n*
